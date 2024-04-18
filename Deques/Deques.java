fcad633e2726caf9ad42d3d5bf09f250d60cb1cc
JIBIN <121844563+JibindasT@users.noreply.github.com>
2024-04-18 12:51:44 +0530

    Completed

---
+++ Deques/Deques.java
@@ -0,0 +1,56 @@
+import java.util.*;
+public class Deques
+{
+public static void main(String[] args)
+{
+Deque<String>deque=new ArrayDeque<String>();
+deque.add("A");
+deque.add("B");
+deque.add("C");
+deque.add("D");
+deque.add("E");
+deque.add("F");
+deque.add("G");
+
+
+
+System.out.print("\nElements in deque	       		         :  ");
+for(String str:deque)
+{
+System.out.print(str+" ");
+}
+
+
+deque.addFirst("Z");
+System.out.print("\n\nQueue after inserting at first  	         :  ");
+for(String str:deque)
+{
+System.out.print(str+" ");
+}
+
+
+deque.addLast("Z");
+System.out.print("\n\nQueue after inserting at last     	         :  ");
+for(String str:deque)
+{
+System.out.print(str+" ");
+}
+
+
+deque.remove();
+System.out.print("\n\nQueue after first element is deleted		 :  ");
+for(String str:deque)
+{
+System.out.print(str+" ");
+}
+
+
+deque.clear();
+deque.add("K\n\n");
+System.out.print("\n\nQueue after clearing and inserting a new element :  ");
+for(String str:deque)
+{
+System.out.print(str+"  ");
+}
+}
+}
